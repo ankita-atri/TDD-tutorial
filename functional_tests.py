ccd2640dfb81985381cde39e290e38d1d16f818d
Ankita Samanta <ankitasa@atrihub.io>
2024-11-14 09:20:34 -0800

    feat (lists): testing the database [ICTRAINING-1779]

--- functional_tests.py
+++ functional_tests.py
@@ -12,38 +12,42 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_test):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_test, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
 
         # checkout the homepage
         self.browser.get('http://localhost:8000')
 
-        # assertEqual, assertTrue, assertFalse
         # check title and header
         self.assertIn("To-Do", self.browser.title)
         header_test = self.browser.find_element(By.TAG_NAME, "h1").text
-
         self.assertIn("To-Do", header_test)
 
         # enter a to-do item
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
-        # type "Buy peacock feathers" into a text box
-        inputbox.send_keys("Buy peacock feathers")
-
-        # hits enter and it updates
+        inputbox.send_keys("1: Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(7)
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows),
-                        "New to-do item did not appear in table"
-                        )
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("2: Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        # time.sleep(1)
 
+        # self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        # self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
 
         self.fail("Finish the test!")
+
 
 
 if __name__ == '__main__':
